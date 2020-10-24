0369c79a6e39d795b0a49125fff131eba0293b9f
Rhusfer John <rcuezon@startechup.com>
2020-10-24 17:37:12 +0800

    Add mongoose

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+// mongodb+srv://admin:P%40ssword01@cluster0.teefr.mongodb.net/my_database?retryWrites=true&w=majority
+// mongodb+srv://admin:P%40ssword01@cluster0.teefr.mongodb.net/my_database?retryWrites=true&w=majority
+
 app.get('/', (req, res) => {
     res.send('Hello Wooorld!!!')
 });
