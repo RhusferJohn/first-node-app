177319762f891770303fd52ef8eb1fa88e611f79
Rhusfer John <rcuezon@startechup.com>
2020-10-24 17:46:37 +0800

    Call db connection on server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000;
-
-// mongodb+srv://admin:P%40ssword01@cluster0.teefr.mongodb.net/my_database?retryWrites=true&w=majority
-// mongodb+srv://admin:P%40ssword01@cluster0.teefr.mongodb.net/my_database?retryWrites=true&w=majority
+const { db } = require('./db/index')
+const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello Wooorld!!!')
-});
+})
+
+db()
 
 app.listen(port, () => {
     console.log('Node server running @ port ', port)
-});
+})
